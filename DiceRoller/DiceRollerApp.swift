637c319ff06153394936fca60810f76a3caef6b0
ZachMommaerts <zmommaerts@gmail.com>
2024-03-11 20:57:45 -0500

    Initial Commit

---
+++ DiceRoller/DiceRollerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceRollerApp.swift
+//  DiceRoller
+//
+//  Created by Zach Mommaerts on 3/11/24.
+//
+
+import SwiftUI
+
+@main
+struct DiceRollerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
